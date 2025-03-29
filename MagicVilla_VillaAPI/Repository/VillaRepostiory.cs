7059650b251ea283096f4c1adb7dacfa6629ca0f
Seif Mohamed <seif.eissa7797@gmail.com>
2025-03-29 21:23:25 +0200

    Adding Villa Repository

---
+++ MagicVilla_VillaAPI/Repository/VillaRepostiory.cs
@@ -0,0 +1,25 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace MagicVilla_VillaAPI.Repository;
+
+public class VillaRepostiory : Repository<Villa>,IVillaRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public VillaRepostiory(ApplicationDbContext context) :base(context)
+    {
+        _context = context;
+    }
+    public async Task<Villa> UpdateAsync(Villa model)
+    {
+        model.UpdatedDate = DateTime.Now;
+        _context.Villas.Update(model);
+        await _context.SaveChangesAsync();
+        return model;
+    }
+
+}
