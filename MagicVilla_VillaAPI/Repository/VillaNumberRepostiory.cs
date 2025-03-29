b15d945ba70e0d5c587e30bd5c24b5d01a70b680
Seif Mohamed <seif.eissa7797@gmail.com>
2025-03-29 21:30:00 +0200

    Add VillaNumber class, repository pattern, and DTOs for villa number management

---
+++ MagicVilla_VillaAPI/Repository/VillaNumberRepostiory.cs
@@ -0,0 +1,24 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace MagicVilla_VillaAPI.Repository;
+
+public class VillaNumberRepostiory : Repository<VillaNumber>,IVillaNumberRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public VillaNumberRepostiory(ApplicationDbContext context) :base(context)
+    {
+        _context = context;
+    }
+    public async Task<VillaNumber> UpdateAsync(VillaNumber model)
+    {
+        model.UpdatedDate = DateTime.Now;
+        _context.VillaNumbers.Update(model);
+        await _context.SaveChangesAsync();
+        return model;
+    }
+}
