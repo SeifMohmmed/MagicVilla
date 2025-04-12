551a1c2576a297179355bc21255565daf086432a
Seif Mohamed <seif.eissa7797@gmail.com>
2025-04-12 20:48:31 +0200

    Fix Misspelling Of ImageUrl

---
+++ MagicVilla_VillaAPI/Migrations/20250412183219_FixMisSpellingOfImageUrl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVilla_VillaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixMisSpellingOfImageUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageURL",
+                table: "Villas",
+                newName: "ImageUrl");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Villas",
+                newName: "ImageURL");
+        }
+    }
+}
