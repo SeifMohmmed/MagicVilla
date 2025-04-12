783df69779ce7b9fea5c7725bcd634b05c49f8da
Seif Mohamed <seif.eissa7797@gmail.com>
2025-04-12 20:12:57 +0200

    Add Content Type for FormFile

--- MagicVilla_Utility/SD.cs
+++ MagicVilla_Utility/SD.cs
@@ -13,4 +13,10 @@ public class SD
     public static string CurrentAPIVersion = "v2";
     public static string Admin = "admin";
     public static string Customer = "customer";
+
+    public enum ContentType
+    {
+        Json,
+        MultipartFormData,
+    }
 }
