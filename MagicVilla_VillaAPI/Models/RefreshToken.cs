1dc7834479de83fd478d8d79820a1b8b0dd4ca8b
Seif Mohamed <seif.eissa7797@gmail.com>
2025-04-15 22:08:48 +0200

    Add Refresh Token to DB

---
+++ MagicVilla_VillaAPI/Models/RefreshToken.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_VillaAPI.Models;
+
+public class RefreshToken
+{
+    [Key]
+    public int Id { get; set; }
+    public string UserId { get; set; }
+    public string JwtTokenId { get; set; } // Unique Token Id that will given to Access Token
+    public string Refresh_Token { get; set; }//Actual Refresh Token
+    public bool IsValid { get; set; }
+    public DateTime ExpiresAt { get; set; }
+}
