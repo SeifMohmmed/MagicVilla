0a147c38ebfa92fea252664bb95036878f398527
Seif Mohamed <seif.eissa7797@gmail.com>
2025-07-06 01:11:03 +0300

    Move Swagger Gen Configuration to Separate File

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -7,8 +7,9 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Text;
 
 namespace MagicVilla_VillaAPI
@@ -100,72 +101,8 @@ namespace MagicVilla_VillaAPI
             //}).AddNewtonsoftJson();
 
             builder.Services.AddAutoMapper(typeof(MappingConfig));
-
-            builder.Services.AddSwaggerGen(options =>
-            {
-                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description =
-                        "JWT Authorization header using the Bearer scheme. \r\n\r\n " +
-                        "Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
-                        "Example: \"Bearer 12345abcdef\"",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Scheme = "Bearer"
-                });
-                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
-                {
-                {
-                     new OpenApiSecurityScheme
-                     {
-                 Reference = new OpenApiReference
-                             {
-                                 Type = ReferenceType.SecurityScheme,
-                                 Id = "Bearer"
-                             },
-                 Scheme = "oauth2",
-                 Name = "Bearer",
-                 In = ParameterLocation.Header
-                     },
-             new List<string>()
-                }
-                });
-                options.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Version = "v1.0",
-                    Title = "Magic Villa",
-                    Description = "API to manage Villa",
-                    TermsOfService = new Uri("https://example.com/terms"),
-                    Contact = new OpenApiContact
-                    {
-                        Name = "Dotnetmastery",
-                        Url = new Uri("https://dotnetmastery.com")
-                    },
-                    License = new OpenApiLicense
-                    {
-                        Name = "Example License",
-                        Url = new Uri("https://example.com/license")
-                    }
-                });
-                options.SwaggerDoc("v2", new OpenApiInfo
-                {
-                    Version = "v2.0",
-                    Title = "Magic Villa V2",
-                    Description = "API to manage Villa",
-                    TermsOfService = new Uri("https://example.com/terms"),
-                    Contact = new OpenApiContact
-                    {
-                        Name = "Dotnetmastery",
-                        Url = new Uri("https://dotnetmastery.com")
-                    },
-                    License = new OpenApiLicense
-                    {
-                        Name = "Example License",
-                        Url = new Uri("https://example.com/license")
-                    }
-                });
-
-            });
+            builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
